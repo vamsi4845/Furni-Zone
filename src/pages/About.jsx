e227955a46273f5300e18253ada1f881b10752cb
vamsisai <vamsisai.karumuri@gmail.com>
2024-05-01 23:58:13 +0530

    chore:Update About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,24 @@
 const About = () => {
-  return <h1 className=""></h1>;
+  return (
+    <>
+      <div className="flex flex-wrap gap-2 sm:gap-x-6  items-center justify-center">
+        <h1 className="text-4xl font-bold leading-none tracking-tight">
+          We love
+        </h1>
+        <div className="stats bg-primary shadow">
+          <div className="stat">
+            <div className="stat-title text-primary-content text-4xl font-bold tracking-widest">
+              Furni Zone
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="mt-6 text-lg leading-8 max-w-2xl mx-auto">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid alias
+        quia impedit beatae fugiat inventore tenetur, at eos accusantium
+        officiis.
+      </p>
+    </>
+  );
 };
 export default About;
