c1dfeef254748e3e4aabf7ca88d962a876c9f58b
vamsisai <vamsisai.karumuri@gmail.com>
2024-05-06 23:11:19 +0530

    chore:Update Home Layout

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,12 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
+import { Navbar, Loading, Header } from "../components";
 
 const HomeLayout = () => {
+  const navigation = useNavigation();
+  const isPageLoading = navigation.state === "loading";
   return (
     <>
-      <nav>
-        <span className="text-4xl text-primary">Furni</span>
-      </nav>
-      <Outlet />
+      <Header />
+      <Navbar />
+      {isPageLoading ? (
+        <Loading />
+      ) : (
+        <section className="align-element py-20">
+          <Outlet />
+        </section>
+      )}
     </>
   );
 };
