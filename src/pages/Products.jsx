a058657edf46fa5432eb6dfdde4fb506c3f1f299
vamsisai <vamsisai.karumuri@gmail.com>
2024-05-24 23:09:04 +0530

    chore:update products

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,4 +1,24 @@
+import { Filters, PaginationContainer, ProductsContainer } from "../components";
+import { customFetch } from "../utils";
+
+const url = "/products";
+export const loader = async ({ request }) => {
+  const params = Object.fromEntries([
+    ...new URL(request.url).searchParams.entries(),
+  ]);
+  const response = await customFetch(url);
+
+  const products = response.data.data;
+  const meta = response.data.meta;
+  return { products, meta, params };
+};
 const Products = () => {
-  return <div>Products</div>;
+  return (
+    <>
+      <Filters />
+      <ProductsContainer />
+      <PaginationContainer />
+    </>
+  );
 };
 export default Products;
