3da8555c43921313be7b1454278d0a3ae21630ed
vamsisai <vamsisai.karumuri@gmail.com>
2024-05-28 23:39:59 +0530

    feat:add Checkout

---
+++ src/components/CheckoutForm.jsx
@@ -0,0 +1,60 @@
+import { Form, redirect } from "react-router-dom";
+import FormInput from "./FormInput";
+import SubmitBtn from "./SubmitBtn";
+import { customFetch, formatPrice } from "../utils";
+import { toast } from "react-toastify";
+import { clearCart } from "../features/cart/cartSlice";
+
+export const action =
+  (store, queryClient) =>
+  async ({ request }) => {
+    const formData = await request.formData();
+    const { name, adress } = await Object.fromEntries(formData);
+    const user = store.getState().userState.user;
+    const { cartItems, orderTotal, numItemsInCart } =
+      store.getState().cartState;
+    const info = {
+      name,
+      adress,
+      chargeTotal: orderTotal,
+      orderTotal: formatPrice(orderTotal),
+      cartItems,
+      numItemsInCart,
+    };
+    try {
+      const response = await customFetch.post(
+        "/orders",
+        { data: indo },
+        {
+          headers: {
+            Authorization: `Bearer${user.token}`,
+          },
+        }
+      );
+      queryClient.removeQueries(["orders"]);
+      store.dispatch(clearCart());
+      toast.success("order places successfully");
+      return redirect("/orders");
+    } catch (error) {
+      console.log(error);
+      const errorMessage =
+        error?.response?.data?.error?.message ||
+        "there was an error placing your order";
+      toast.error(errorMessage);
+      return null;
+    }
+  };
+
+const CheckoutForm = () => {
+  return (
+    <Form method="POST" className="flex flex-xol gap-y-4">
+      <h4 className="font-medium text-xl">Shipping Information</h4>
+      <FormInput label="first name" name="name" type="text" />
+      <FormInput label="adress" name="adress" type="text" />
+      <div className="mt-4">
+        <SubmitBtn text="Place Your Order" />
+      </div>
+    </Form>
+  );
+};
+export default CheckoutForm;
