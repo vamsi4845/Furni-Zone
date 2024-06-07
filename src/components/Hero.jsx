562c896d66c618d3ab567c86bdfe8c1c8c665964
vamsisai <vamsisai.karumuri@gmail.com>
2024-06-07 23:09:34 +0530

    refractor:update landing page text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,14 +6,15 @@ import { Link } from "react-router-dom";
 const carouselImages = [hero1, hero2, hero3, hero4];
 const Hero = () => {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-24 items-center">
+    <div className="grid lg:grid-cols-2 gap-24 items-center">
       <div>
-        <h1 className="max-w-2xl text-4xl font-bold tracking-tight sm:text-6x.">
-          We're changing the way people shope
+        <h1 className="max-w-2xl text-4xl font-bold tracking-tight sm:text-6xl">
+          Transform Your Home with Style
         </h1>
         <p className="mt-8 max-w-xl text-lg leading-8">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-          Voluptatibus, perspiciatis
+          Discover our wide range of modern and classic furniture pieces crafted
+          to add comfort and style to any room. Quality and affordability
+          guaranteed
         </p>
         <div className="mt-10">
           <Link to="/products" className="btn btn-primary">
@@ -24,7 +25,7 @@ const Hero = () => {
       <div className="hidden h-[28rem] lg:carousel carousel-center p-4 space-x-4 bg-neutral rounded-box">
         {carouselImages.map((image, index) => {
           return (
-            <div key={image} className="carousel-item">
+            <div key={index} className="carousel-item">
               <img
                 src={image}
                 className="rounded-box h-full w-80 object-cover"
